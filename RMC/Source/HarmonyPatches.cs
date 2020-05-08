b1a0e506e5c27a3f82f1d5a3c6f0264e8ee3f106
codrod <codrod63@gmail.com>
2020-05-08 00:02:58 -0400

    Fixed bug which caused the world map to freeze

--- RMC/Source/HarmonyPatches.cs
+++ RMC/Source/HarmonyPatches.cs
@@ -15,6 +15,7 @@ using HarmonyLib;
  * 1) The pawn generator class only takes Factions not FactionDefs. Which casues a problem if you try mix armies since the faction def in the visting armies pawn kind def does not exist. will
  * probably have to something drastic to fix this.
  * 2) Generated backstories may contradict the forced (disallowed) traits/work tags if the backstories are not forced?
+ * 3) ISC should not have relatives!
  */
 
 namespace RMC
