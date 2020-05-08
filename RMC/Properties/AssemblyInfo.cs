cc9507dabf45a17e5ec0123ef87763cdf7159f96
codrod <codrod63@gmail.com>
2020-05-08 01:27:19 -0400

    Updated assembly info

--- RMC/Properties/AssemblyInfo.cs
+++ RMC/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RMC")]
+[assembly: AssemblyTitle("RimWorld Military Colony")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RMC")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("RimWorld Military Colony")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
