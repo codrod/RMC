9d994fe118e1bca2298d9386a5928b2b65f3cac3
codrod <codrod63@gmail.com>
2020-05-08 00:13:06 -0400

    Clean up comments

--- RMC/Source/HarmonyPatches.cs
+++ RMC/Source/HarmonyPatches.cs
@@ -11,11 +11,16 @@ using System.Linq;
 using HarmonyLib;
 
 /*
- * Notes:
- * 1) The pawn generator class only takes Factions not FactionDefs. Which causes a problem if you try mix armies since the faction def in the visiting armies pawn kind def does not exist. will
- * probably have to something drastic to fix this.
- * 2) Generated back stories may contradict the forced (disallowed) traits/work tags if the back stories are not forced?
- * 3) ISC should not have relatives!
+ * Bugs:
+ * 1) The pawn generator class only takes Factions not FactionDefs. Which causes a problem if you try to mix armies
+ * because if a custom faction def is used it wont exist. So the default faction will be used instead.
+ * 
+ * 2) Generated back stories may contradict the forced (disallowed) traits/work tags if both back stories are not forced?
+ * In general mixing forced back stories with random causes problems. It is better to just force both childhood and adulthood
+ * instead of just one.
+ * 
+ * 3) ISC should not have relatives! I noticed that royal tribute collector had a guard with a relation to one of the spacemarines
+ * Should see if there is a solution that problem but there might not be.
  */
 
 namespace RMC
