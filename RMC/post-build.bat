d84c3574c870e9a7b563063433888afa6ea01776
Cody Rodgers <rodgers@rdacorp.com>
2021-07-08 09:45:55 -0400

    Fix actions?

--- RMC/post-build.bat
+++ RMC/post-build.bat
@@ -1,16 +1,14 @@
-REM This can be changed if need be
-set RimWorldDir=C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods
 set Version=1.2
 set ConfigurationName=%1
 set ProjectDir=%2
 
 xcopy /yi .\RMC*.dll "RimWorld Military Colony\%Version%\Assemblies"
-echo "%ProjectDir%\Assets\XML"
 xcopy /yei "%ProjectDir%\Assets\XML" "RimWorld Military Colony\%Version%"
 xcopy /yei "%ProjectDir%\Assets\About" "RimWorld Military Colony\About"
 
 REM If in debug then deploy to local rimworld install
 if "%ConfigurationName%" == "Debug" (
+call "%ProjectDir%\local-settings.bat"
 del /s /q "%RimWorldDir%\RimWorld Military Colony"
 xcopy /yei "RimWorld Military Colony" "%RimWorldDir%\RimWorld Military Colony"
 xcopy /yei "..\..\Assets\Scenarios" "%userprofile%\AppData\LocalLow\Ludeon Studios\RimWorld by Ludeon Studios\Scenarios"
